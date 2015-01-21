ce4b825d409e750797cc19dd73e1c272ebb76547
AndreyRudenko <strelec2222@rambler.ru>
2015-01-21 19:45:43 +0200

    Initial commit Hello

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+
+public class Main {
+    static int indexOF(int h, int length) {
+        return h & (length -1);
+    }
+
+    public static void main(String[] args) {
+    }
+
+
+}
---
+++ src/com/company/Person.java
@@ -0,0 +1,52 @@
+package com.company;
+
+/**
+ * Created by student on 14.01.2015.
+ */
+public class Person {
+    String name ;
+    int age;
+    int weight;
+    int height;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        if (height != person.height) return false;
+        if (weight != person.weight) return false;
+        if (!name.equals(person.name)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age;
+        result = 31 * result + weight;
+        result = 31 * result + height;
+        return result;
+    }
+
+    public Person() {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", height=" + height +
+                '}';
+    }
+}
